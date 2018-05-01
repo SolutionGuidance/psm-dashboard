fe6abd10cef167047ca2edf3c650dff4b5aba38b
Paul Morris <paul@paulwmorris.com>
2018-05-01 16:04:44 -0400

    Remove temporary randomization of feature dates and status

--- dashboard.js
+++ dashboard.js
@@ -1,33 +1,6 @@
 (function(d3) {
   "use strict";
 
-  function randomizeFeature(feature) {
-    var random = Math.random() * 3;
-    if (random < 1) {
-      feature.status = "Completed";
-      if (random < 0.5) {
-        feature.startDate = "2018-02-01";
-        feature.completedDate = "2018-04-01";
-      } else {
-        feature.startDate = "2018-03-01";
-        feature.completedDate = "2018-04-01";
-      }
-    } else if (random > 2) {
-      feature.status = "InProgress";
-      if (random < 2.5) {
-        feature.startDate = "2018-03-01";
-      } else {
-        feature.startDate = "2018-04-01";
-      }
-    }
-  }
-
-  function randomizeFeatures(features) {
-    Object.keys(features).forEach(function(featureId) {
-      randomizeFeature(features[featureId])
-    });
-  }
-
   function readJsonFile(file, callback) {
     var rawFile = new XMLHttpRequest();
     rawFile.overrideMimeType("application/json");
@@ -42,10 +15,6 @@
 
   readJsonFile("sample-input.json", function(text) {
     var data = JSON.parse(text);
-
-    // A temporary measure. 
-    randomizeFeatures(data.features)
-
     window.drawFeaturesPieChart(data, d3);
     window.drawFeaturesBurnDownChart(data, d3);
   });
